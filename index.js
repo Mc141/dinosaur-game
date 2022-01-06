8701ae748f6865f04573dbdb73686e92f93dc667
Mc141 <85196947+Mc141@users.noreply.github.com>
2022-01-06 14:54:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,8 @@ document.addEventListener("keydown", (e) => {
 });
 
 document.addEventListener("click", (e) => {
-  if (e.key === " ") {
     dino.classList.add("jump");
     setTimeout(removeClass, 500);
-  }
 });
 
 setInterval(checkIfDead, 1);
